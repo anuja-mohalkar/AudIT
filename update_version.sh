2c7a30c0058a1301dd0dd705bac0225dc2035559
afour <afour@afour-virtual-machine>
2022-09-29 10:30:46 +0530

    bash file audit1.sh and update_version.sh are modified

---
+++ update_version.sh
@@ -0,0 +1,33 @@
+# EDIT
+
+VERSION="1"
+
+SCRIPT_URL='https://github.com/anuja-mohalkar/AudIT/blob/main/audit1.sh'
+
+
+
+# DO NOT EDIT
+
+SCRIPT_LOCATION="${BASH_SOURCE[@]}"
+
+ABS_SCRIPT_PATH=$(readlink -f "$SCRIPT_LOCATION")
+
+TMP_FILE=$(mktemp -p "" "test.sh")
+
+
+
+curl -s -L "$SCRIPT_URL" > "$TMP_FILE"
+
+NEW_VER=$(grep "^VERSION" "$TMP_FILE" | awk -F'[="]' '{print $3}')
+
+if [[ "$VERSION" < "$NEW_VER" ]]; then
+
+    printf "Updating script \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "$VERSION" "$NEW_VER"
+
+    cp -f "$TMP_FILE" "$ABS_SCRIPT_PATH" || printf "Unable to update the script"
+
+else
+
+     printf "Already the latest version."
+
+fi
