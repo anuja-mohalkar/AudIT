ba4bf548994b62d704ee0dde583024ffff093d00
anuja-mohalkar <114467411+anuja-mohalkar@users.noreply.github.com>
2022-10-11 22:22:25 +0530

    Update audit1.sh

--- audit1.sh
+++ audit1.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+
 VERSION="2"
+SCRIPT_URL='https://raw.githubusercontent.com/anuja-mohalkar/AudIT/main/audit1.sh'
+SCRIPT_LOCATION="${BASH_SOURCE[@]}"
+ABS_SCRIPT_PATH=$(readlink -f "$SCRIPT_LOCATION")
+TMP_FILE=$(mktemp -p "" "XXXXX.sh")
+
+curl -s -L "$SCRIPT_URL" > "$TMP_FILE"
+NEW_VER=$(grep "^VERSION" "$TMP_FILE" | awk -F'[="]' '{print $3}')
+if [[ "$VERSION" < "$NEW_VER" ]]; then
+    printf "Updating script \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "$VERSION" "$NEW_VER"
+    cp -f "$TMP_FILE" "$ABS_SCRIPT_PATH" || printf "Unable to update the script"
+else
+     printf "Already the latest version."
+fi
+
 temp_line_count=1
 echo "--------------------------------------------"
 
